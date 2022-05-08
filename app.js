ff7161bc8eb928db12da9809d48bff2b46554f04
Jepoy Factoran <factoran20@gmail.com>
2022-05-08 21:18:03 +0800

    post method creating data

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
+//API used https://jsonplaceholder.typicode.com/users
+//this api is used just for learning purposes
+
+//Create instance of the ezhttp class
 const user = new EzHttp();
-user
-  .get("https://jsonplaceholder.typicode.com/users")
-  .then((user) => console.log(user))
-  .catch((err) => console.log(err));
+//GET USER API
+// user
+//   .get("https://jsonplaceholder.typicode.com/users")
+//   .then((user) => console.log(user))
+//   .catch((err) => console.log(err));
+
+// const data = {
+//   name: "jepoy",
+//   username: "jepoysanity",
+//   email: "factoran20@gmail.com",
+// };
+
+//CREATE USER API
+// user
+//   .post("https://jsonplaceholder.typicode.com/users", data)
+//   .then((user) => console.log(user))
+//   .catch((err) => console.log(err));
--- ezhttp.js
+++ ezhttp.js
@@ -7,4 +7,18 @@ class EzHttp {
         .catch((err) => reject(err));
     });
   }
+  post(url, data) {
+    return new Promise((resolve, reject) => {
+      fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(data),
+      })
+        .then((res) => res.json())
+        .then((user) => resolve(user))
+        .catch((err) => reject(err));
+    });
+  }
 }
