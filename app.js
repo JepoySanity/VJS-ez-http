84941e8429e98f510d0b5ae32bb5b20616097fd2
Jepoy Factoran <factoran20@gmail.com>
2022-05-08 21:25:20 +0800

    put method for updating data

--- app.js
+++ app.js
@@ -3,20 +3,27 @@
 
 //Create instance of the ezhttp class
 const user = new EzHttp();
+//test data for post and put request
+const data = {
+  name: "jepoy",
+  username: "jepoysanity",
+  email: "factoran20@gmail.com",
+};
+
 //GET USER API
 // user
 //   .get("https://jsonplaceholder.typicode.com/users")
 //   .then((user) => console.log(user))
 //   .catch((err) => console.log(err));
 
-// const data = {
-//   name: "jepoy",
-//   username: "jepoysanity",
-//   email: "factoran20@gmail.com",
-// };
-
 //CREATE USER API
 // user
 //   .post("https://jsonplaceholder.typicode.com/users", data)
 //   .then((user) => console.log(user))
 //   .catch((err) => console.log(err));
+
+//UPDATE USER API
+user
+  .put("https://jsonplaceholder.typicode.com/users/2", data)
+  .then((user) => console.log(user))
+  .catch((err) => console.log(err));
--- ezhttp.js
+++ ezhttp.js
@@ -1,4 +1,5 @@
 class EzHttp {
+  //get request to api
   get(url) {
     return new Promise((resolve, reject) => {
       fetch(url)
@@ -7,10 +8,26 @@ class EzHttp {
         .catch((err) => reject(err));
     });
   }
+  //post request to api
   post(url, data) {
     return new Promise((resolve, reject) => {
       fetch(url, {
         method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(data),
+      })
+        .then((res) => res.json())
+        .then((user) => resolve(user))
+        .catch((err) => reject(err));
+    });
+  }
+  //update request to api
+  put(url, data) {
+    return new Promise((resolve, reject) => {
+      fetch(url, {
+        method: "PUT",
         headers: {
           "Content-type": "application/json",
         },
